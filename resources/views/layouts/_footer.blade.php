5a0289a6fa11d70f9deb626d85d8b10d311ec990
keith孙 <459179970@qq.com>
2022-10-13 11:48:37 +0800

    用户显示页面

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer">
-  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" width="17px" height="17px">
   <a href="https://learnku.com/laravel/courses" target=_blank>
     刻意练习，每日精进
   </a>
